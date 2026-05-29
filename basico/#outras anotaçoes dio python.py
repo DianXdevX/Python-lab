ba81b2ae0df0e3d06a68feb85938e4f3e8f31cb8
DIANA ARAUJO <ld.diana.araujo@gmail.com>
2026-05-29 12:17:13 -0300

    dio notes

---
+++ basico/#outras anotaçoes dio python.py
@@ -0,0 +1,27 @@
+#outras anotaçoes  dio python 
+#modo interativo 
+#chamar python  ou executar com a flag i  : python-i app.py (app.py = nome do arquivo)
+            
+                # dir  e help #
+#dir  retorna a lsita de nome  do local atual  pode ter ou nao argumento 
+#ex dir ()   dir(object) - ex dir(1)
+#help =sistema  de ajuda  integrado  expica o que retorna e como fuciona 
+#help()  help(object)- ex help(1)
+
+                 #constante em python
+#pyton nao tem  uma palavra reservada para constante 
+# ha uma convesao que constante  com nome ALL maiusculas e uma constante    
+
+                # conversao 
+#basta usar o construtor da  conversao desejada 
+#exemplos 
+print(int(1))
+print(int("10"))
+print(float ("10.10"))
+#ou 
+print (100 /2) #float
+print (100//)# int
+
+                #entradas e saidas
+            #input e a funçao   que e sempre formatado em str  //entrada
+            #print   mistra o valor /saida 
