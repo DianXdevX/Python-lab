351996079aa1a2b02c21587f0b1c0e99fbf9f799
DIANA ARAUJO <ld.diana.araujo@gmail.com>
2026-05-29 15:32:02 -0300

    dio notes

--- basico/#outras anotaçoes dio python.py
+++ basico/#outras anotaçoes dio python.py
@@ -23,5 +23,30 @@ print (100 /2) #float
 print (100//)# int
 
                 #entradas e saidas
-            #input e a funçao   que e sempre formatado em str  //entrada
-            #print   mistra o valor /saida 
+#input e a funçao   que e sempre formatado em str  //entrada
+#print   mistra o valor /saida 
+
+            #if else ternario 
+#  var =  namevar  if  condiçao  else  saidaelse
+#ex status = 'suceso if saldp>= saque else ""falha
+
+            #  strings
+          # strin de  mutiplas linhas = """xxx -"""" - preserva a formataçao
+          #upper  = converte maisulo
+          #lower =  =minusculo 
+          #title  = 1maiscula restominuscula 
+          #strip = remove o espaços brancos 
+          #rstrup= remove  branco direita
+          #lstrip = remove branco esquerdo
+          #center  =centraliza   string.ccenter (10,"#")
+          #join  adiciona algo ("string"join(curso)) 
+        
+        #interpolaçao de strings
+        # %s = string , %d= int  , %f = float ( declara variaves)
+        # format {}   onde uqe colocar as var  e no fim;format(var names)
+        # f-strint {nome da variavel}- o mais usado e pode ser formatado { var . format }
+
+        #fatiamento de strings 
+        # retorna substring 
+        # [star , stop, step ]  (step  de 1 e um ou 2 e 2 )
+        #ex [10:16:2 ]
